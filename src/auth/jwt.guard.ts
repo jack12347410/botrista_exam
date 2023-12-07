64c5146c221a23ed746642c3b738bba97de01c0f
jack12347410 <jack12347410@gmail.com>
2023-12-07 09:47:59 +0800

    clean

--- src/auth/jwt.guard.ts
+++ src/auth/jwt.guard.ts
@@ -1,7 +1,5 @@
 import { Injectable, ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Reflector } from '@nestjs/core';
-import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {}
